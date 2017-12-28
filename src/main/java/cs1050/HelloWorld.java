9407b8f98820ecf358d488857035290350847a42
Tomer <shaolintl@gmail.com>
2017-12-28 13:04:06 +0100

    initial template

--- src/main/java/cs1050/HelloWorld.java
+++ src/main/java/cs1050/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.klarna;
+package cs1050;
 
 public class HelloWorld {
     public static void main (String[] args) {
--- src/test/java/cs1050/HelloWorldTest.java
+++ src/test/java/cs1050/HelloWorldTest.java
@@ -1,14 +1,14 @@
-package com.klarna;
+package cs1050;
 
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class HelloWorldTest {
-    
+public class H elloWorldTest {
+
     @Test
-    public void testSomething() {
+    public void  testSomething() {
         assertThat(1 == 1, is(true));
     }
 }
