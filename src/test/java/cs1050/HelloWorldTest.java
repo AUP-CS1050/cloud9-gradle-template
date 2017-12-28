922255b482fee106ca47f7d27a0c97336364dcd5
Tomer <shaolintl@gmail.com>
2017-12-28 21:15:06 +0100

    template ready

--- src/test/java/cs1050/HelloWorldTest.java
+++ src/test/java/cs1050/HelloWorldTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class H elloWorldTest {
+public class HelloWorldTest {
 
     @Test
     public void  testSomething() {
