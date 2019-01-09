3d46eda5dc9d6de8c62b8bcbb7ccfba12e59a1fa
Tomer <shaolintl@gmail.com>
2019-01-09 21:28:38 +0100

    updated gradle to 5.1

--- install-gradle.sh
+++ install-gradle.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ~
-wget https://services.gradle.org/distributions/gradle-4.5-rc-1-bin.zip
-unzip gradle-4.5-rc-1-bin.zip
-rm -f gradle-4.5-rc-1-bin.zip
+wget https://services.gradle.org/distributions/gradle-5.1-bin.zip
+unzip gradle-5.1-bin.zip
+rm -f gradle-5.1-bin.zip
 
-echo 'GRADLE_HOME=~/gradle-4.5-rc-1' >> ~/.profile
+echo 'GRADLE_HOME=~/gradle-5.1' >> ~/.profile
 echo 'PATH=$GRADLE_HOME/bin:$PATH' >> ~/.profile
 source ~/.profile
 cd workspace
